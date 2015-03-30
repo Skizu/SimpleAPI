58aa40642b7179d78b448325c50593a4c8bd3885
Daniel Hunt <daniel@brokertech.co.uk>
2015-03-30 14:35:59 +0100

    Add composer requirements and initial API wrapper

---
+++ src/Skizu/SimpleAPI/RegisterAPI.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace Skizu\SimpleAPI;
+
+use App\Http\Controllers\Controller;
+use GuzzleHttp\Client;
+use Cache;
+
+Class LookupAPIThrottleException extends \Exception
+{
+}
+
+Class LookupAPIConfigException extends \Exception
+{
+}
+
+class RegisterAPI extends Controller
+{
+    const THROTTLE_LIMIT = 100;
+    const STORAGE_TIME = 1440;
+    const THROTTLE_TIME = 60;
+    const CACHE_TIME = 60;
+
+    private $method = 'GET';
+    private $limit;
+    private $storage_time;
+    private $cache_key;
+    private $cache_time;
+
+    public function __construct($api)
+    {
+        $this->api = strtoupper($api);
+
+        $api_url_key = $this->api . '_API_URL';
+        $this->api_url = env($api_url_key);
+
+        $this->limit = env($this->api . '_API_THROTTLE_LIMIT', self::THROTTLE_LIMIT);
+        $this->storage_time = env($this->api . '_API_STORAGE_TIME', self::STORAGE_TIME);
+        $this->cache_time = env($this->api . '_API_CACHE_TIME', self::CACHE_TIME);
+        $this->queries_key = $this->api . '.count';
+
+        if (!isset($this->api_url) || empty($this->api_url)) {
+            throw new LookupAPIConfigException("Unable to find: $api_url_key");
+        }
+    }
+
+    public function action($action)
+    {
+        $this->api_url .= $action;
+        return $this;
+    }
+
+    public function lookup($search = NULL, $method = 'GET')
+    {
+        $this->cache_key = $this->cacheKey($search);
+        $this->method = $method;
+
+        return Cache::get($this->cache_key, function () use ($method, $search) {
+            $this->throttleCheck();
+            return $this->start($search);
+        });
+    }
+
+    private function start($search)
+    {
+        return $this->store($this->queryAPI($search));
+    }
+
+    private function queryAPI($search)
+    {
+        $client = new Client();
+
+        $request = $client->createRequest($this->method, $this->api_url);
+        if ($search) $request->setQuery($search);
+
+        return $client->send($request);
+    }
+
+    private function throttleCheck()
+    {
+        if ($this->limit < $this->queries()) {
+            throw new LookupAPIThrottleException('API throttled due to flood of requests');
+        }
+    }
+
+    private function cacheKey($search)
+    {
+        return md5($this->api_url . serialize($search));
+    }
+
+    private function store($response)
+    {
+        $result = $this->detectType($response);
+        Cache::add($this->cache_key, $result, $this->storage_time);
+        Cache::add($this->queries_key, $this->queries() + 1, $this->cache_time);
+
+        return $result;
+    }
+
+    private function detectType(\GuzzleHttp\Message\Response $response)
+    {
+        switch ($response->getHeader('content-type')) {
+            case 'application/json':
+                return $response->json();
+                break;
+
+            case 'text/xml':
+                return json_decode(json_encode($response->xml()), true);
+                break;
+
+            default:
+                return $response->getBody();
+        }
+
+    }
+
+    private function queries()
+    {
+        return Cache::get($this->queries_key, 0);
+    }
+}
